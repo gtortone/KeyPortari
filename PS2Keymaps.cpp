4e4b9e6529e404a5c19e592716d39888f330c02b
Firmaplus(+) Ltd <w.stubig@firmaplus.de>
2025-03-29 01:49:20 +0100

    firmware v0.0.1

---
+++ PS2Keymaps.cpp
@@ -0,0 +1,177 @@
+/*
+  PS2Keymaps.cpp - PS2Keymaps library
+  Copyright (c) 2025 Free Software Foundation.  All right reserved.
+  Written by Wolfgang Stubig <w.stubig@firmaplus.de> 2025
+
+  extracted from PS2Keyboard library written by Christian Weichel <info@32leaves.net>
+  for more information you can read the original wiki in arduino.cc
+  at http://www.arduino.cc/playground/Main/PS2Keyboard
+  or http://www.pjrc.com/teensy/td_libs_PS2Keyboard.html
+*/
+
+// http://www.quadibloc.com/comp/scan.htm
+// http://www.computer-engineering.org/ps2keyboard/scancodes2.html
+
+// These arrays provide a simple key map, to turn scan codes into ISO8859
+// output.  For US, DE and FR keyboards.
+//
+
+#include "PS2Keymaps.h"
+
+const PROGMEM PS2Keymap_t PS2Keymap_US = {
+  // without shift
+	{0, PS2_F9, 0, PS2_F5, PS2_F3, PS2_F1, PS2_F2, PS2_F12,
+	0, PS2_F10, PS2_F8, PS2_F6, PS2_F4, PS2_TAB, '`', 0,
+	0, 0 /*Lalt*/, 0 /*Lshift*/, 0, 0 /*Lctrl*/, 'q', '1', 0,
+	0, 0, 'z', 's', 'a', 'w', '2', 0,
+	0, 'c', 'x', 'd', 'e', '4', '3', 0,
+	0, ' ', 'v', 'f', 't', 'r', '5', 0,
+	0, 'n', 'b', 'h', 'g', 'y', '6', 0,
+	0, 0, 'm', 'j', 'u', '7', '8', 0,
+	0, ',', 'k', 'i', 'o', '0', '9', 0,
+	0, '.', '/', 'l', ';', 'p', '-', 0,
+	0, 0, '\'', 0, '[', '=', 0, 0,
+	0 /*CapsLock*/, 0 /*Rshift*/, PS2_ENTER /*Enter*/, ']', 0, '\\', 0, 0,
+	0, 0, 0, 0, 0, 0, PS2_BACKSPACE, 0,
+	0, '1', 0, '4', '7', 0, 0, 0,
+	'0', '.', '2', '5', '6', '8', PS2_ESC, 0 /*NumLock*/,
+	PS2_F11, '+', '3', '-', '*', '9', PS2_SCROLL, 0,
+	0, 0, 0, PS2_F7 },
+  // with shift
+	{0, PS2_F9, 0, PS2_F5, PS2_F3, PS2_F1, PS2_F2, PS2_F12,
+	0, PS2_F10, PS2_F8, PS2_F6, PS2_F4, PS2_TAB, '~', 0,
+	0, 0 /*Lalt*/, 0 /*Lshift*/, 0, 0 /*Lctrl*/, 'Q', '!', 0,
+	0, 0, 'Z', 'S', 'A', 'W', '@', 0,
+	0, 'C', 'X', 'D', 'E', '$', '#', 0,
+	0, ' ', 'V', 'F', 'T', 'R', '%', 0,
+	0, 'N', 'B', 'H', 'G', 'Y', '^', 0,
+	0, 0, 'M', 'J', 'U', '&', '*', 0,
+	0, '<', 'K', 'I', 'O', ')', '(', 0,
+	0, '>', '?', 'L', ':', 'P', '_', 0,
+	0, 0, '"', 0, '{', '+', 0, 0,
+	0 /*CapsLock*/, 0 /*Rshift*/, PS2_ENTER /*Enter*/, '}', 0, '|', 0, 0,
+	0, 0, 0, 0, 0, 0, PS2_BACKSPACE, 0,
+	0, '1', 0, '4', '7', 0, 0, 0,
+	'0', '.', '2', '5', '6', '8', PS2_ESC, 0 /*NumLock*/,
+	PS2_F11, '+', '3', '-', '*', '9', PS2_SCROLL, 0,
+	0, 0, 0, PS2_F7 },
+	0
+};
+
+
+const PROGMEM PS2Keymap_t PS2Keymap_German = {
+  // without shift
+	{0, PS2_F9, 0, PS2_F5, PS2_F3, PS2_F1, PS2_F2, PS2_F12,
+	0, PS2_F10, PS2_F8, PS2_F6, PS2_F4, PS2_TAB, '^', 0,
+	0, 0 /*Lalt*/, 0 /*Lshift*/, 0, 0 /*Lctrl*/, 'q', '1', 0,
+	0, 0, 'y', 's', 'a', 'w', '2', 0,
+	0, 'c', 'x', 'd', 'e', '4', '3', 0,
+	0, ' ', 'v', 'f', 't', 'r', '5', 0,
+	0, 'n', 'b', 'h', 'g', 'z', '6', 0,
+	0, 0, 'm', 'j', 'u', '7', '8', 0,
+	0, ',', 'k', 'i', 'o', '0', '9', 0,
+	0, '.', '-', 'l', PS2_o_DIAERESIS, 'p', PS2_SHARP_S, 0,
+	0, 0, PS2_a_DIAERESIS, 0, PS2_u_DIAERESIS, '\'', 0, 0,
+	0 /*CapsLock*/, 0 /*Rshift*/, PS2_ENTER /*Enter*/, '+', 0, '#', 0, 0,
+	0, '<', 0, 0, 0, 0, PS2_BACKSPACE, 0,
+	0, '1', 0, '4', '7', 0, 0, 0,
+	'0', '.', '2', '5', '6', '8', PS2_ESC, 0 /*NumLock*/,
+	PS2_F11, '+', '3', '-', '*', '9', PS2_SCROLL, 0,
+	0, 0, 0, PS2_F7 },
+  // with shift
+	{0, PS2_F9, 0, PS2_F5, PS2_F3, PS2_F1, PS2_F2, PS2_F12,
+	0, PS2_F10, PS2_F8, PS2_F6, PS2_F4, PS2_TAB, PS2_DEGREE_SIGN, 0,
+	0, 0 /*Lalt*/, 0 /*Lshift*/, 0, 0 /*Lctrl*/, 'Q', '!', 0,
+	0, 0, 'Y', 'S', 'A', 'W', '"', 0,
+	0, 'C', 'X', 'D', 'E', '$', PS2_SECTION_SIGN, 0,
+	0, ' ', 'V', 'F', 'T', 'R', '%', 0,
+	0, 'N', 'B', 'H', 'G', 'Z', '&', 0,
+	0, 0, 'M', 'J', 'U', '/', '(', 0,
+	0, ';', 'K', 'I', 'O', '=', ')', 0,
+	0, ':', '_', 'L', PS2_O_DIAERESIS, 'P', '?', 0,
+	0, 0, PS2_A_DIAERESIS, 0, PS2_U_DIAERESIS, '`', 0, 0,
+	0 /*CapsLock*/, 0 /*Rshift*/, PS2_ENTER /*Enter*/, '*', 0, '\'', 0, 0,
+	0, '>', 0, 0, 0, 0, PS2_BACKSPACE, 0,
+	0, '1', 0, '4', '7', 0, 0, 0,
+	'0', '.', '2', '5', '6', '8', PS2_ESC, 0 /*NumLock*/,
+	PS2_F11, '+', '3', '-', '*', '9', PS2_SCROLL, 0,
+	0, 0, 0, PS2_F7 },
+	1,
+  // with altgr
+	{0, PS2_F9, 0, PS2_F5, PS2_F3, PS2_F1, PS2_F2, PS2_F12,
+	0, PS2_F10, PS2_F8, PS2_F6, PS2_F4, PS2_TAB, 0, 0,
+	0, 0 /*Lalt*/, 0 /*Lshift*/, 0, 0 /*Lctrl*/, '@', 0, 0,
+	0, 0, 0, 0, 0, 0, PS2_SUPERSCRIPT_TWO, 0,
+	0, 0, 0, 0, PS2_CURRENCY_SIGN, 0, PS2_SUPERSCRIPT_THREE, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, PS2_MICRO_SIGN, 0, 0, '{', '[', 0,
+	0, 0, 0, 0, 0, '}', ']', 0,
+	0, 0, 0, 0, 0, 0, '\\', 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0 /*CapsLock*/, 0 /*Rshift*/, PS2_ENTER /*Enter*/, '~', 0, '#', 0, 0,
+	0, '|', 0, 0, 0, 0, PS2_BACKSPACE, 0,
+	0, '1', 0, '4', '7', 0, 0, 0,
+	'0', '.', '2', '5', '6', '8', PS2_ESC, 0 /*NumLock*/,
+	PS2_F11, '+', '3', '-', '*', '9', PS2_SCROLL, 0,
+	0, 0, 0, PS2_F7 }
+};
+
+
+const PROGMEM PS2Keymap_t PS2Keymap_French = {
+  // without shift
+	{0, PS2_F9, 0, PS2_F5, PS2_F3, PS2_F1, PS2_F2, PS2_F12,
+	0, PS2_F10, PS2_F8, PS2_F6, PS2_F4, PS2_TAB, PS2_SUPERSCRIPT_TWO, 0,
+	0, 0 /*Lalt*/, 0 /*Lshift*/, 0, 0 /*Lctrl*/, 'a', '&', 0,
+	0, 0, 'w', 's', 'q', 'z', PS2_e_ACUTE, 0,
+	0, 'c', 'x', 'd', 'e', '\'', '"', 0,
+	0, ' ', 'v', 'f', 't', 'r', '(', 0,
+	0, 'n', 'b', 'h', 'g', 'y', '-', 0,
+	0, 0, ',', 'j', 'u', PS2_e_GRAVE, '_', 0,
+	0, ';', 'k', 'i', 'o', PS2_a_GRAVE, PS2_c_CEDILLA, 0,
+	0, ':', '!', 'l', 'm', 'p', ')', 0,
+	0, 0, PS2_u_GRAVE, 0, '^', '=', 0, 0,
+	0 /*CapsLock*/, 0 /*Rshift*/, PS2_ENTER /*Enter*/, '$', 0, '*', 0, 0,
+	0, '<', 0, 0, 0, 0, PS2_BACKSPACE, 0,
+	0, '1', 0, '4', '7', 0, 0, 0,
+	'0', '.', '2', '5', '6', '8', PS2_ESC, 0 /*NumLock*/,
+	PS2_F11, '+', '3', '-', '*', '9', PS2_SCROLL, 0,
+	0, 0, 0, PS2_F7 },
+  // with shift
+	{0, PS2_F9, 0, PS2_F5, PS2_F3, PS2_F1, PS2_F2, PS2_F12,
+	0, PS2_F10, PS2_F8, PS2_F6, PS2_F4, PS2_TAB, 0, 0,
+	0, 0 /*Lalt*/, 0 /*Lshift*/, 0, 0 /*Lctrl*/, 'A', '1', 0,
+	0, 0, 'W', 'S', 'Q', 'Z', '2', 0,
+	0, 'C', 'X', 'D', 'E', '4', '3', 0,
+	0, ' ', 'V', 'F', 'T', 'R', '5', 0,
+	0, 'N', 'B', 'H', 'G', 'Y', '6', 0,
+	0, 0, '?', 'J', 'U', '7', '8', 0,
+	0, '.', 'K', 'I', 'O', '0', '9', 0,
+	0, '/', PS2_SECTION_SIGN, 'L', 'M', 'P', PS2_DEGREE_SIGN, 0,
+	0, 0, '%', 0, PS2_DIAERESIS, '+', 0, 0,
+	0 /*CapsLock*/, 0 /*Rshift*/, PS2_ENTER /*Enter*/, PS2_POUND_SIGN, 0, PS2_MICRO_SIGN, 0, 0,
+	0, '>', 0, 0, 0, 0, PS2_BACKSPACE, 0,
+	0, '1', 0, '4', '7', 0, 0, 0,
+	'0', '.', '2', '5', '6', '8', PS2_ESC, 0 /*NumLock*/,
+	PS2_F11, '+', '3', '-', '*', '9', PS2_SCROLL, 0,
+	0, 0, 0, PS2_F7 },
+	1,
+  // with altgr
+	{0, PS2_F9, 0, PS2_F5, PS2_F3, PS2_F1, PS2_F2, PS2_F12,
+	0, PS2_F10, PS2_F8, PS2_F6, PS2_F4, PS2_TAB, 0, 0,
+	0, 0 /*Lalt*/, 0 /*Lshift*/, 0, 0 /*Lctrl*/, '@', 0, 0,
+	0, 0, 0, 0, 0, 0, '~', 0,
+	0, 0, 0, 0, 0 /*PS2_EURO_SIGN*/, '{', '#', 0,
+	0, 0, 0, 0, 0, 0, '[', 0,
+	0, 0, 0, 0, 0, 0, '|', 0,
+	0, 0, 0, 0, 0, '`', '\\', 0,
+	0, 0, 0, 0, 0, '@', '^', 0,
+	0, 0, 0, 0, 0, 0, ']', 0,
+	0, 0, 0, 0, 0, 0, '}', 0,
+	0 /*CapsLock*/, 0 /*Rshift*/, PS2_ENTER /*Enter*/, '¤', 0, '#', 0, 0,
+	0, '|', 0, 0, 0, 0, PS2_BACKSPACE, 0,
+	0, '1', 0, '4', '7', 0, 0, 0,
+	'0', '.', '2', '5', '6', '8', PS2_ESC, 0 /*NumLock*/,
+	PS2_F11, '+', '3', '-', '*', '9', PS2_SCROLL, 0,
+	0, 0, 0, PS2_F7 }
+};
--- PS2Keyoboard_1.ino
+++ PS2Keyoboard_1.ino
@@ -1,108 +1,79 @@
 /*
-docs/sources:
-https://www.sciencebuddies.org/science-fair-projects/references/ascii-table
-https://wolles-elektronikkiste.de/en/interrupts-part-2-pin-change-interrupts
-https://www.hackster.io/michalin70/connect-a-usb-keyboard-with-an-arduino-50c077
-https://www.youtube.com/watch?v=Bv_zbPUtAuQ
 
 todo:
- - Add multiple keymaps (DE, US, ??) use switchblock 2-3 GPIOs (4-8 Keymaps)
- - Arrow keys = left joystick 
- - ASCII except "xxxx 1111" right port nibble
- - Add ShiftKey (0b00010010 left 0b01011001 right)
+ - Init process !
+ - Add TJs ASCII mapping
+ - I2C protocol ?
+
 */
-#include <unordered_map>
 
 #include "PS2Keymaps.h"
 
-#define DEBUG           1
-
-#define KEY_BACKSPACE   2
-#define KEY_ENTER       3
-#define KEY_SHIFT_1     3
-#define KEY_SHIFT_2     3
-
-#define KEY_UP          0b11101111  // same than left jostick up
-#define KEY_DOWN        0b11011111  // same than left jostick down
-#define KEY_LEFT        0b10111111  // same than left jostick left
-#define KEY_RIGHT       0b01111111  // same than left jostick right
+#define DEBUG        0
 
 // Arduino Pins
-#define CLOCK       9 //D-
-#define DATA        8 //D+
-#define LED_AND_OE 13
+#define CLOCK              9 // USB D-
+#define DATA               8 // USB D+
+#define LED_AND_OE        13
+#define DIP_A_PIN         A0
 
-//#define BREAK     0x01
-#define MODIFIER  0x01
-#define SHIFT     0x02
-#define ALTGR     0x04
+// Flags for keyboard_state
+#define MODIFIER        0x01
+#define SHIFT           0x02
+#define ALTGR           0x04
 
-// keymap for 24 char display
-const char keymap2[][132] = {{
- /*   0 */  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
- /*  16 */  4, 4, 4, 4, 4, 176, 220, 4, 4, 4, 212, 184, 112, 200, 224, 4, 
- /*  32 */  4, 120, 204, 124, 128, 232, 228, 4, 4, 4, 196, 132, 188, 180, 236, 4, 
- /*  48 */  4, 164, 116, 140, 136, 208, 240, 4, 4, 4, 160, 148, 192, 244, 248, 4, 
- /*  64 */  4, 4, 152, 144, 168, 216, 252, 4, 4, 1, 4, 156, 4, 172, 0, 4, 
- /*  80 */  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, KEY_ENTER, 4, 4, 4, 4, 4, 
- /*  96 */  4, 4, 4, 4, 4, 4, KEY_BACKSPACE, 4, 4, 220, 4, 232, 244, 4, 4, 4, 
- /* 112 */  216, 1, 224, 236, 240, 248, 4, 4, 4, 4, 228, 0, 4, 252, 4, 4, 
- /* 128 */  4, 4, 4, 4
-},{ // US Keyboard Layout map
-    0,  0,   0,  0,  0,  0,  0, 0, 0, 0,  0,  0,  0,  0, '`', 0,
-    0,  0,   0,  0,  0,'q','1', 0, 0, 0, 'z','s','a','w','2', 0,
-    0,'c', 'x','d','e','4','3', 0, 0,' ','v','f','t','r','5', 0,
-    0,'n', 'b','h','g','y','6', 0, 0, 0, 'm','j','u','7','8', 0,
-    0,',', 'k','i','o','0','9', 0, 0,'.','/','l',';','p','-', 0,
-    0,  0,'\'',  0,'[','=',  0, 0, 0, 0,13, ']', 0, '\\', 0, 0,
-    0,  0,   0,  0,  0,  0,127, 0, 0,'1', 0,'4','7', 0, 0, 0,
-  '0','.', '2','5','6','8',  0, 0, 0,'+','3','-','*','9', 0, 0,
-    0,  0,  0,  0
-}
-};
+// 24 Char display 'special' key values
+#define BACKSPACE_24CHAR   2
+#define ENTER_24CHAR       3
+
+#define LEFT_JOYSTICK_UP          0b11101111
+#define LEFT_JOYSTICK_DOWN        0b11011111
+#define LEFT_JOYSTICK_LEFT        0b10111111
+#define LEFT_JOYSTICK_RIGHT       0b01111111
+
 
 uint8_t getChar24Id(char c) {
   uint8_t o = (uint8_t)c;
 
-  // Großbuchstaben (A-Z)
+  // Upper case chars (A-Z)
   if(o > 64 && o < 91) {
-    return (o - 63) * 4;  // Beispiel: 'A' (65) -> (65 - 63) * 4 = 8
+    return (o - 63) * 4;  // e.g. 'A' (65) -> (65 - 63) * 4 = 8
   }
-  // Kleinbuchstaben (a-z)
+  // Lower case chars (a-z)
   if(o > 96 && o < 123) {
-    return (o - 69) * 4;  // Beispiel: 'a' (97) -> (97 - 69) * 4 = 112
+    return (o - 69) * 4;  // e.g. 'a' (97) -> (97 - 69) * 4 = 112
   }
-  // Ziffern (0-9)
+  // Digits (0-9)
   if(o > 47 && o < 58) {
-    return (o + 6) * 4;   // Beispiel: '0' (48) -> (48 + 6) * 4 = 216
+    return (o + 6) * 4;   // e.g. '0' (48) -> (48 + 6) * 4 = 216
   }
-  // Sonderfälle
+  // Other
   switch(o) {
-    case 32:  // Leerstelle
-      return 4;
-    case 46:  // Punkt
+    case 46:  // Dot
       return 1;
-    case 45:  // Minus
+    case 44:  // Comma
       return 0;
+    case PS2_UPARROW:
+      return LEFT_JOYSTICK_UP;
+    case PS2_LEFTARROW:
+      return LEFT_JOYSTICK_LEFT;
+    case PS2_DOWNARROW:
+      return LEFT_JOYSTICK_DOWN;
+    case PS2_RIGHTARROW:
+      return LEFT_JOYSTICK_RIGHT;
+    case PS2_BACKSPACE: // or PS2_DELETE
+      return BACKSPACE_24CHAR;
+    case PS2_ENTER:
+      return ENTER_24CHAR;
     default:
-      return 4;
+      return 4; // Default is Space
   }
 }
 
 void set_port_input(){
-  // set RIOT ports to input
+  // set port on RIOT bus to input
   DDRD = 0b00000000;
   PORTD = 0b00000000; // floating, no pullups
-/*
-  pinMode(PORTD0, INPUT); // D0
-  pinMode(PORTD1, INPUT); // D1
-  pinMode(PORTD2, INPUT); // D2
-  pinMode(PORTD3, INPUT); // D3
-  pinMode(PORTD4, INPUT); // D4
-  pinMode(PORTD5, INPUT); // D5
-  pinMode(PORTD6, INPUT); // D6
-  pinMode(PORTD7, INPUT); // D7
-*/
 }
 
 void set_port_output(char value){
@@ -115,36 +86,45 @@ void set_port_output(char value){
   Serial.print(value);
   Serial.println(" down");
   #else
-  // todo find Char24 or TJ for value
   DDRD = 0b11111111;
-  PORTD = value;
+  // todo mapping/protocol specific
+  PORTD = getChar24Id(value);
   #endif
 }
+
+uint8_t dipConfig;
+PS2Keymap_t *keymap;
 
 void setup() {
   #if DEBUG
   Serial.begin(115200);
-  #else
-  set_port_input();
   #endif
+
   pinMode(CLOCK, INPUT_PULLUP); // For most keyboards the builtin pullups are sufficient, so the 10k pullups can be omitted
   pinMode(DATA, INPUT_PULLUP);
   pinMode(LED_AND_OE, OUTPUT);
-
-
   digitalWrite(LED_AND_OE, LOW); // Activate joystick forwarding via transceiver (e.g. SN74LVC245AN)
 
   bitSet(PCICR, PCIE0);   // Enable pin change interrupts on pin B0-B7
   bitSet(PCMSK0, PCINT1); // Pin change interrupt on Clock pin (PCINT1 = PB1 Arduino Pin 9)
 
+  int adcValue = analogRead(DIP_A_PIN);      // Read voltage of DIP-Switch block resistor ladder (0-1023)
+  dipConfig = map(adcValue, 0, 1023, 15, 0); // Get binary value of DIP-Switch Block (0-15) 
+ 
+  switch (dipConfig & 0b11) { // lower 2 bits of dipConfig define keyboard layout
+    case 0b01: keymap = &PS2Keymap_German;      break;
+    case 0b10: keymap = &PS2Keymap_French;      break;
+    default: keymap = &PS2Keymap_US;            break;
+  }
+
   #if DEBUG
   Serial.println("Keyboard test");
+  Serial.println(dipConfig);
   #endif
 }
 
 uint8_t curscan = 0;
 bool key_action = false;
-const PS2Keymap_t *keymap = &PS2Keymap_German;
 
 ISR(PCINT0_vect) {
   uint16_t newscan = 0;
@@ -161,13 +141,14 @@ ISR(PCINT0_vect) {
   bitSet(PCIFR, PCIF0);
 }
 
-int init_step = 0;
-bool init_done = true;
-unsigned long init_timer = 0;
 uint8_t lastscan = 0;
 
 void loop() {
- 	static uint8_t state = 0;
+ 	static uint8_t keyboard_state = 0;
+  int init_step = 0;
+  bool init_done = true;
+  unsigned long init_timer = 0;
+  uint8_t busConfig = 0;
   char c;
 /*
   if(init_step == 0 && (PORTD & 0x0F) == 0){
@@ -178,19 +159,22 @@ void loop() {
   }else if(init_step == 2 && (PORTD & 0x0F) == 0){
     init_step++;
   }else if(init_step == 3 && (PORTD & 0x0F) != 0){
+    uint8_t busConfig = (PORTD & 0x0F);
     init_step++;
+    delay(20); // 16.6667 one 60 Hz frame
+    set_port_output(busConfig); // return busConfig for confirmation on the bus
+    delay(20); // 16.6667 one 60 Hz frame
     init_done = true;
   }
 */
   if(init_done){
-
     if(key_action){
       key_action = false;
       if(curscan != 0xF0 && curscan != 0xE0){
  				if (curscan < PS2_KEYMAP_SIZE){
-          if (state & SHIFT) {
+          if (keyboard_state & SHIFT) {
             c = pgm_read_byte(keymap->shift + curscan);
-          } else if ((state & ALTGR) && keymap->uses_altgr) {
+          } else if ((keyboard_state & ALTGR) && keymap->uses_altgr) {
             c = pgm_read_byte(keymap->altgr + curscan);
           } else {
             c = pgm_read_byte(keymap->noshift + curscan);
@@ -199,9 +183,9 @@ void loop() {
 
         if(lastscan == 0xF0){
           if (curscan == 0x12 || curscan == 0x59) {
-            state &= ~SHIFT;
-          } else if (curscan == 0x11 && (state & MODIFIER)) {
-            state &= ~ALTGR;
+            keyboard_state &= ~SHIFT;
+          } else if (curscan == 0x11 && (keyboard_state & MODIFIER)) {
+            keyboard_state &= ~ALTGR;
           } else {
             // Key up
             // 1. set data Port to input
@@ -217,15 +201,14 @@ void loop() {
           }
           // CTRL, ALT & WIN keys could be added
           // but is that really worth the overhead?
-          state &= ~MODIFIER; //~(BREAK | MODIFIER);
+          keyboard_state &= ~MODIFIER;
         }else{
           if (curscan == 0x12 || curscan == 0x59) {
-            state |= SHIFT;
-          } else if (curscan == 0x11 && (state & MODIFIER)) {
-            state |= ALTGR;
+            keyboard_state |= SHIFT;
+          } else if (curscan == 0x11 && (keyboard_state & MODIFIER)) {
+            keyboard_state |= ALTGR;
           } else {
             if(lastscan == 0xE0){ // MODIFIER
-            Serial.println("MODIFIER");
               switch (curscan) {
                 case 0x70: c = PS2_INSERT;      break;
                 case 0x6C: c = PS2_HOME;        break;
@@ -233,7 +216,7 @@ void loop() {
                 case 0x71: c = PS2_DELETE;      break;
                 case 0x69: c = PS2_END;         break;
                 case 0x7A: c = PS2_PAGEDOWN;    break;
-                case 0x75: c = 'A';     break; // PS2_UPARROW
+                case 0x75: c = PS2_UPARROW;     break;
                 case 0x6B: c = PS2_LEFTARROW;   break;
                 case 0x72: c = PS2_DOWNARROW;   break;
                 case 0x74: c = PS2_RIGHTARROW;  break;
@@ -245,7 +228,7 @@ void loop() {
             // Call key-down of active protocol here!
             set_port_output(c);
           }
-    			state &= ~MODIFIER;// ~(BREAK | MODIFIER);
+    			keyboard_state &= ~MODIFIER;
         }
       }
       lastscan = curscan;
