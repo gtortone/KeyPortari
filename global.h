b327bb3db69d16c5299cbc024424c0f0e50d10f1
Firmaplus(+) Ltd. <w.stubig@firmaplus.de>
2025-04-10 15:42:41 +0200

    Global header file

---
+++ global.h
@@ -0,0 +1,19 @@
+/*
+ * Global defines for the Project
+*/
+
+
+
+#define DEBUG        0
+
+// Arduino Pins
+#define CLOCK              9 // USB D-
+#define DATA               8 // USB D+
+#define OE_LEFT_PORT      12
+#define OE_RIGHT_PORT     13
+#define DIP_A_PIN         A0
+
+// Flags for keyboard_state
+#define MODIFIER        0x01
+#define SHIFT           0x02
+#define ALTGR           0x04
