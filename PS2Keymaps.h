625919a0765c4e728721cc2feae5882a9a408ecb
Firmaplus(+) Ltd. <w.stubig@firmaplus.de>
2025-05-11 00:11:30 +0200

    Cleanup, keycodes for F1-F6,F10,F11

--- PS2Keymaps.h
+++ PS2Keymaps.h
@@ -9,33 +9,33 @@
 
 // These configure what byte will be returned
 // for each "special" key.  To ignore a key, use zero.
-#define PS2_TAB				9
-#define PS2_ENTER			13
-#define PS2_BACKSPACE			127
-#define PS2_ESC				27
-#define PS2_INSERT			0
-#define PS2_DELETE			127
-#define PS2_HOME			0
-#define PS2_END				0
-#define PS2_PAGEUP			25
-#define PS2_PAGEDOWN			26
-#define PS2_UPARROW			11
-#define PS2_LEFTARROW			8
-#define PS2_DOWNARROW			10
-#define PS2_RIGHTARROW			21
-#define PS2_F1				0
-#define PS2_F2				0
-#define PS2_F3				0
-#define PS2_F4				0
-#define PS2_F5				0
-#define PS2_F6				0
-#define PS2_F7				0
-#define PS2_F8				0
-#define PS2_F9				0
-#define PS2_F10				0
-#define PS2_F11				0
-#define PS2_F12				0
-#define PS2_SCROLL			0
+#define PS2_TAB              9
+#define PS2_ENTER           13
+#define PS2_BACKSPACE      127
+#define PS2_ESC             27
+#define PS2_INSERT           0
+#define PS2_DELETE         127
+#define PS2_HOME             2
+#define PS2_END              3
+#define PS2_PAGEUP          25
+#define PS2_PAGEDOWN        26
+#define PS2_UPARROW         11
+#define PS2_LEFTARROW        8
+#define PS2_DOWNARROW       10
+#define PS2_RIGHTARROW      21
+#define PS2_F1              14
+#define PS2_F2              15
+#define PS2_F3              16
+#define PS2_F4              17
+#define PS2_F5              18
+#define PS2_F6              19
+#define PS2_F7               0
+#define PS2_F8               0
+#define PS2_F9               0
+#define PS2_F10             20
+#define PS2_F11             22
+#define PS2_F12              0
+#define PS2_SCROLL           0
 
 #define PS2_INVERTED_EXCLAMATION	161 // ¡
 #define PS2_CENT_SIGN			162 // ¢
@@ -67,7 +67,7 @@
 #define PS2_FRACTION_ONE_QUARTER	188 // ¼
 #define PS2_FRACTION_ONE_HALF		189 // ½
 #define PS2_FRACTION_THREE_QUARTERS	190 // ¾
-#define PS2_INVERTED_QUESTION MARK	191 // ¿
+#define PS2_INVERTED_QUESTION_MARK	191 // ¿
 #define PS2_A_GRAVE			192 // À
 #define PS2_A_ACUTE			193 // Á
 #define PS2_A_CIRCUMFLEX		194 // Â
